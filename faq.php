8af3ba8cb2d610c2185c3ccafd7254a6e5732571
Manu Cornet <manu@manucornet.net>
2015-06-05 20:33:38 -0700

    Fix an English oddity

--- faq.php
+++ faq.php
@@ -46,7 +46,7 @@ trying to make more people get to know about Linux.</p>
 stupid!</b></a></p>
 
 <p>A: On a website for advanced Linux users, details like this would matter,
-however I am wanting to keep things fairly simple. People heard of "Linux", not 
+however I want to keep things fairly simple. People have heard of "Linux", not 
 "GNU/Linux", and this kind of geeky quarrel is probably one of the 
 things that prevent "normal" people from understanding what this is all 
 about. For the same reason, I write "OpenOffice" instead of 
