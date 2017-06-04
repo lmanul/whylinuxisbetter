c4ab5a73b96af9f384b7934e5ef89402d64ebd93
Manu Cornet <manucornet@google.com>
2017-06-04 15:34:33 -0700

    Update license

--- licence.php
+++ licence.php
@@ -1,3 +1,1 @@
-<div>
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/il/deed.en"><img alt="Creative Commons Licence" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/2.5/il/88x31.png" title="Creative Commons Licence" /></a><br /><span dir="rtl">This work is licenced under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/il/">Creative Commons Attribution-ShareAlike 2.5 Israel Licence</a>.</span>
-</div>
+https://creativecommons.org/licenses/by-nc-nd/4.0/
